cb07d348399e777602a14857c8adfff1c7150ae0
tobiascasazza <tobiascasazza@gmail.com>
2022-03-06 09:19:21 -0300

    Servicio

---
+++ MotorDeBusqueda-servicio/MotorDeBusquedaBackEnd/Models/Prenda.cs
@@ -0,0 +1,12 @@
+﻿namespace MotorDeBusqueda.Models
+{
+    public class Prenda
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Talle { get; set; }
+        public int Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Foto { get; set; }
+    }
+}
