97df0180d5d6f177266506952e186fb1625f5bff
tobiascasazza <tobiascasazza@gmail.com>
2022-03-08 20:18:58 -0300

    Error de puertos solucionado

---
+++ MotorDeBusqueda-servicio/MotorDeBusqueda/Controllers/PrendaController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MotorDeBusqueda.Data;
+using MotorDeBusqueda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorDeBusqueda.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PrendaController : ControllerBase
+    {
+
+        private readonly ILogger<PrendaController> _logger;
+
+        public PrendaController(ILogger<PrendaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Prenda> GetPrendas()
+        {
+            IEnumerable<Prenda> resultado = PrendaData.Datos();
+            List<Prenda> list = PrendaData.Datos();
+            return resultado;
+        }
+
+        [HttpGet("id/{id}")]
+        public Prenda GetPrendaById(int id)
+        {   Prenda resultado = new Prenda();
+            List<Prenda> list = PrendaData.Datos();
+            foreach(Prenda prenda in list)
+            {
+                if(prenda.Id == id)
+                {
+                    resultado = prenda;
+                }
+            }
+            return resultado;
+        }
+        [HttpGet("nombre/{nombre}")]
+        public IEnumerable<Prenda> GetPrendaByNombre(string nombre)
+        {
+            List<Prenda> resultado = new List<Prenda>();
+            List<Prenda> list = PrendaData.Datos();
+            foreach (Prenda prenda in list)
+            {
+                if (prenda.Nombre.ToLower().Contains(nombre.ToLower()))
+                {
+                    resultado.Add(prenda);
+                }
+            }
+            return resultado;
+        }
+
+    }
+}
